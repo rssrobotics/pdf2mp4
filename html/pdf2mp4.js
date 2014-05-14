42c52149f81b6a8fad340a628887489099ad0e70
Edwin Olson <eblson@umich.edu>
2014-05-14 02:55:39 +0000

    wip

--- html/pdf2mp4.js
+++ html/pdf2mp4.js
@@ -18,7 +18,7 @@ function xmlhttp_post(url, data, callback)
                 if (xmlhttp.status==200)
                     callback(xmlhttp, url);
                 else
-                    alert("XMLHTTP request to "+url+" got status "+xmlhttp.status+". Cross-domain request?");
+                    alert("XMLHTTP request to "+url+" got status "+xmlhttp.statusText+". Cross-domain request?");
             }
         };
 
@@ -31,28 +31,6 @@ function xmlhttp_post(url, data, callback)
 
     xmlhttp.send(data);
 }
-
-/*
-function doc_create(nslides)
-{
-    doc = new Object();
-    doc.slides = [];
-
-    for (i = 0; i < nslides; i++) {
-        var slide = new Object();
-        slide['type'] = "slide";
-        slide['thumb'] = "pdfthumb-"+(i+1)+".png";
-        slide['seconds'] = Math.floor(5*60 / nslides);
-
-        doc.slides.push(slide);
-    }
-
-    doc.display_idx = 0;
-
-    doc_rebuild_gui();
-    doc_save();
-}
-*/
 
 // deserialize a saved document
 function doc_restore(json_string)
@@ -171,7 +149,7 @@ function doc_rebuild_gui()
         for (var i = 0; i < doc.slides.length; i++) {
             var slide = doc.slides[i];
 
-            slide_nav.innerHTML += "<img id=slide_nav_"+i+" width="+(slide_nav.offsetWidth-40)+" class=thumb onclick='doc_set_display_idx("+i+", false)' src="+project_url+"/"+ slide.thumb + "><br>";
+            slide_nav.innerHTML += "<img id=slide_nav_"+i+" width="+(slide_nav.offsetWidth-40)+" class=thumb onclick='doc_set_display_idx("+i+", false)' src="+project_url+"/"+ slide.thumb + "><br><span class=slide_nav_time id=slide_nav_time_"+i+">"+slide.seconds+"</span>";
         }
 
 	window.onresize = function () {
@@ -185,6 +163,7 @@ function doc_rebuild_gui()
         var f = parseFloat(document.getElementById("slide_seconds").value);
         if (isFinite(f)) {
             doc.slides[doc.display_idx].seconds = Math.max(0, f);
+	    document.getElementById("slide_nav_time_"+doc.display_idx).innerHTML=doc.slides[doc.display_idx].seconds;
             doc_recompute_total_seconds();
         }
         doc_save();
