ac722bd414f3e8cb52d2cd69a67e1e6a72cfb47d
Edwin Olson <eblson@umich.edu>
2014-05-17 02:05:15 +0000

    make movie upload prettier

--- html/project_add_movie.php
+++ html/project_add_movie.php
@@ -9,6 +9,10 @@ validate_key($key);
 
 ?>
 
+<H2>Movie Upload</h2>
+
+<P>Select your MP4 file and click upload. Note that the	maximum	movie size is 100MB and/or 1000 frames, and that any audio data will be discarded. 
+
 <form enctype="multipart/form-data" method=post action=project_add_movie_submit.php>
 <input type=hidden name=key value="<?php print $key ?>">
 <input type=hidden name=idx value="<?php print $_REQUEST["idx"] ?>">
@@ -17,7 +21,7 @@ Movie: <input type=file size=50 name=moviefile><br>
 <input type=hidden name=mode value="hold">
 
 <br>
-<input type=submit>
+    <input type=image src=upload.png name=submit>
 
 </form>
 
